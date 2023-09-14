8496ec6ae62de069781dbbef0320a6ff87e54ddf
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-09-14 15:49:28 +0200

    Create particles5.js

---
+++ dbrAin/paris/particles5.js
@@ -0,0 +1,96 @@
+particlesJS('particles-js', {
+ 'particles': {
+ 'number': {
+ 'value': 89,
+ 'density': {
+ 'enable': true,
+ 'value_area': 800
+ }
+ },
+ 'color': { 'value': '#ff0266',},
+ 'shape': {
+ 'type': 'image',
+ 'stroke': {
+ 'width': 3,
+ 'color': '#000000'
+ },
+ 'edge': { 'nb_sides': 5 },
+ 'image': {
+ 'src': 'https://aibolem.github.io/dbrein/FrontEnd/TAR_PARIS.png',
+ 'width': 18,
+ 'height': 18
+ }
+ },
+ 'opacity': {
+ 'value': 0.5,
+ 'random': true,
+ 'anim': {
+ 'enable': true,
+ 'speed': 1,
+ 'opacity_min': 0.1,
+ 'sync': false
+ }
+ },
+ 'size': {
+ 'value': 18,
+ 'random': true,
+ 'anim': {
+ 'enable': true,
+ 'speed': 1,
+ 'size_min': 18,
+ 'sync': true
+ }
+ },
+ 'line_linked': {
+ 'enable': true,
+ 'distance': 150,
+ 'color': "#03dac6",
+ 'opacity': 0.4,
+ 'width': 2
+ },
+ 'move': {
+ 'enable': true,
+ 'speed': 2,
+ 'direction': 'none',
+ 'random': true,
+ 'straight': false,
+ 'out_mode': 'out',
+ 'bounce': true,
+ 'attract': {
+ 'enable': true,
+ 'rotateX': 600,
+ 'rotateY': 1200
+ }
+ }
+ },
+ 'interactivity': {
+ 'detect_on': 'canvas',
+ 'events': {
+     'onhover': {
+ 'enable': true,
+ 'mode': 'grab'
+ },
+ 'resize': true
+ },
+ 'modes': {
+ 'grab': {
+ 'distance': 140,
+ 'line_linked': { 'opacity': 1 }
+ },
+ 'bubble': {
+ 'distance': 300,
+ 'size': 18,
+ 'duration': 2,
+ 'opacity': 8,
+ 'speed': 1
+ },
+ 'repulse': {
+ 'distance': 140,
+ 'duration': 0.4
+ },
+ 'push': { 'particles_nb': 4 },
+ 'remove': { 'particles_nb': 2 }
+ }
+ },
+ 'retina_detect': true
+ });
