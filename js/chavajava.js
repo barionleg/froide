e6c1e5f31cbac1be29a5c8a59869c83a97906e7c
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-09-14 14:43:23 +0200

    Create chavajava.js

---
+++ js/chavajava.js
@@ -0,0 +1,6 @@
+<iframe id="chyiframe" src="https://aibolem.github.io/dbrein/paris/index.html"></iframe>
+<script>
+   var iframe = document.getElementById("chyiframe");
+   iframe.width = iframe.contentWindow.document.body.scrollWidth;
+   iframe.height = iframe.contentWindow.document.body.scrollHeight;
+</script>
