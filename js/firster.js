e1d88781b20aebbed70ec119050bf1e195be4a99
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-09-30 20:58:25 +0200

    Update firster.js

--- js/firster.js
+++ js/firster.js
@@ -7,6 +7,5 @@
     return true;
 }</script>
 
-<a href="https://barionleg.github.io/death-star/public/index.html" onclick="return loadIframe('frame1', this.href)">swi</a>
-    <iframe name="frame1" id="firster" src="https://barionleg.github.io/death-star/public/index.html" frameborder="0">
-    Your browser doesn't support iframes.</iframe>
+<a href="https://barionleg.github.io/death-star/public/index.html" onclick="return loadIframe('frame1', this.href)"></a>
+
